e0796d193540b17cba356749fe1a6c9530606e0e
uje <jmujeda@gmail.com>
2015-07-07 11:14:32 -0300

    Avances en clase Jueguito y loop principal, todavía no está terminado

--- headers/Jueguito.h
+++ headers/Jueguito.h
@@ -1,12 +1,27 @@
 #ifndef __Jueguito__
 #define __Jueguito__
+#include <SDL2/SDL.h>
+#include <iostream>
 
-
+/**
+ * Clase principal
+ * bool isRunning mantiene el loop del juego
+ * Doc: The Game Class p21 
+ */
 class Jueguito {
 public:
-	Jueguito(){}
-	~Jueguito(){}
+	Jueguito();
+	~Jueguito();
+	void init();
+	void render();
+	void handleEvents();
+	void update();
+	void clean();
+	bool getIsRunning();
+	void setIsRunning(bool estado);
 private:
-
+	bool isRunning;
+	SDL_Window* window;
+	SDL_Renderer* renderer;
 };
 #endif
--- src/Jueguito.cpp
+++ src/Jueguito.cpp
@@ -1,1 +1,74 @@
 #include "../headers/Jueguito.h"
+
+
+Jueguito::Jueguito() {
+	
+}
+
+Jueguito::~Jueguito() {
+
+}
+
+
+void Jueguito::init() {
+	
+	// initialize SDL
+	if(SDL_Init(SDL_INIT_EVERYTHING) >= 0){
+		setIsRunning(true);
+
+		/**
+		 * Crear métodos propioos para construir la ventana y el renderer
+		 */
+		
+		// if succeeded create our window
+		this->window = SDL_CreateWindow("Chapter 1:Setting up SDL", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN);
+		// if the window creation succeeded create our renderer
+		if(this->window) {
+			this->renderer = SDL_CreateRenderer(this->window, -1, 0);
+			if (this->renderer) {
+				//std::cout << "render init ok" << std::endl;
+				// everything succeeded lets draw the window	
+				// This function expects Red, Green, Blue and
+				// Alpha as color values
+				SDL_SetRenderDrawColor(this->renderer, 255, 0, 0, 255);
+			}	
+		}
+		
+		//std::cout << "running true" << std::endl;
+		
+	
+	}
+	else {
+		std::cout << "sdl could not initialize" << std::cout;
+		setIsRunning(false); // sdl could not initialize
+	}
+}
+
+void Jueguito::render() {
+		// clear the window to black
+	SDL_RenderClear(this->renderer);
+	// show the window
+	SDL_RenderPresent(this->renderer);
+
+
+}
+
+void Jueguito::update() {
+
+}
+
+void Jueguito::clean() {
+
+}
+
+void Jueguito::handleEvents() {
+	
+}
+
+bool Jueguito::getIsRunning() {
+	return this->isRunning;
+}
+
+void Jueguito::setIsRunning(bool estado) {
+	this->isRunning = estado;
+}
